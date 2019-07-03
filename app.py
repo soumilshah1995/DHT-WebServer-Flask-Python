b0e9671ac9fc59712ff462a6ce6b75a6b308eaad
soumilshah1995 <soushah@my.bridgeport.edu>
2019-07-03 19:20:06 -0400

    project

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import render_template, url_for, request
+import Adafruit_DHT
+pin = 17                        // Connected to GPIO 17
+sensor = Adafruit_DHT.DHT11     // using DHT-11 Sensor
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    temperature, humidity = sensor_1()
+    return render_template("sensor.html",temperature=temperature, humidity=humidity)
+
+def sensor_1():
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    if humidity is not None and temperature is not None:
+        return temperature, humidity
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
